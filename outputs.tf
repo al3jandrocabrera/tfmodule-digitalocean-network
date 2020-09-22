0263df1ae1a66befc97665de39f32630be053c25
Alejandro Cabrera <alejandrocabrera.inc@gmail.com>
2020-09-21 22:57:48 -0600

    Added uuid and urn outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "tag_name" {
   value = digitalocean_tag.this.name
 }
+
+output "vpc_uuid" {
+  value = digitalocean_vpc.this.id
+}
+
+output "vpc_urn" {
+  value = digitalocean_vpc.this.urn
+}
